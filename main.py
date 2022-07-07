fa0725f5f63146381d04498723a27f2ac1a541ec
Siddhant2601 <deshmukhsiddhant420@gmail.com>
2022-07-06 21:46:00 -0500

    full code uploaded

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file
+from flask_qrcode import QRcode
+import qrcode
+
+
 app = Flask(__name__)
+QRcode(app)
 
 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")\
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        text_ = request.form.get("fname")
+        data = request.args.get("data", "")
+        url_ = 
+    return render_template("index.html")
 
 
-
-@app.route('/result')
-def result():
-    return render_template("result.html")
+@app.route("/qrcode", methods=["GET"])
+def get_qrcode():
+    # please get /qrcode?data=<qrcode_data>
+    data = request.args.get("data", "")
+    return send_file(qrcode(data, mode="raw"), mimetype="image/png")
 
 
 if __name__ == '__main__':
